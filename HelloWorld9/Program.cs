225979cb8c818b27a2d3d77885b91ca34c1c9743
Robertspoon <robertthemcjug@gmail.com>
2022-09-13 09:13:43 -0300

    Initial commit

---
+++ HelloWorld9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 9");
+            Console.WriteLine("By Robert Atkinson");
+            Console.ReadKey(true);
+        }
+    }
+}
